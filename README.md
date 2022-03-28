5d6a91c4094ea640375a2e93f5950fa0d10b3a68
Mike L <myclues@gmail.com>
2022-03-28 10:54:56 -0400

    add screenshots and update instructions

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 ## What to code
 
-The exercise is to build a simple front-end web application that has two screens: a user sign-up form, and a user admin screen.
+The exercise is to build a simple front-end web application that has two screens: a user sign-up form, and a user admin screen.  This should take you between 2-4 hours to complete.
+
+Some sample screenshots have been provided but you are welcome to use your own design instead.  You do not need to create any custom graphics.  Feel free to utilize CSS frameworks if you prefer.
+
+![New user screen](examples/new_user.png)
 
 The user sign-up form should
 
@@ -11,6 +15,8 @@ The user sign-up form should
 - Validate the email - a simple rule that follows `x@y.z` format
 - A submission should be rejected if the email address is already in use
 
+![User admin screen](examples/user_list.png)
+
 The user admin screen should
 
 - Display each user's info in a simple format.  You can use a table, cards, or anything else you'd like.
@@ -18,7 +24,7 @@ The user admin screen should
 
 ## Doing the exercise
 
-This repo is based on the vanilla `create-react-app` template that is meant to help you get started, but feel free to use a different boilerplate or start with a clean slate.  You shouldn't need to, but you may also add additional NPM libraries as you like.  It is often just as important to be able to combine various open source libraries into a cohesive product.
+This repo is based on the vanilla `create-react-app` template that is meant to help you get started, but feel free to use a different boilerplate or start with a clean slate.  You may also add additional NPM libraries as you like.  It is often just as important to be able to combine various open source libraries into a cohesive product.
 
 It is preferred for you to complete this exercise using ReactJS, but if you are unfamiliar, you are also welcome to use other frameworks like Angular or Vue.
 
--- src/App.js
+++ src/App.js
@@ -4,36 +4,46 @@ const App = () => (
   <div className="App">
     <h1>Frontend Interview Exercise</h1>
 
-    <h2>What to code</h2>
-    <p>The exercise is to build a simple front-end web application that has two screens: a user sign-up form, and a user admin screen.</p>
+    <section>
+      <h2>What to code</h2>
+      <p>The exercise is to build a simple front-end web application that has two screens: a user sign-up form, and a user admin screen.  This should take you between 2-4 hours to complete.</p>
 
-    <p>The user sign-up form should</p>
+      <p>Some sample screenshots have been provided but you are welcome to use your own design instead.  You do not need to create any custom graphics.  Feel free to utilize CSS frameworks if you prefer.</p>
 
-    <ul>
-      <li>Accept the first name, last name, nickname, and email address</li>
-      <li>Each of the inputs is required except nickname</li>
-      <li>Validate the email - a simple rule that follows <code>x@y.z</code> format</li>
-      <li>A submission should be rejected if the email address is already in use</li>
-    </ul>
+      <p>See <code>/examples/new_user.png</code> in the repo for a sample screenshot.</p>
 
-    <p>The user admin screen should</p>
+      <p>The user sign-up form should</p>
 
-    <ul>
-      <li>Display each user's info in a simple format.  You can use a table, cards, or anything else you'd like.</li>
-    </ul>
+      <ul>
+        <li>Accept the first name, last name, nickname, and email address</li>
+        <li>Each of the inputs is required except nickname</li>
+        <li>Validate the email - a simple rule that follows <code>x@y.z</code> format</li>
+        <li>A submission should be rejected if the email address is already in use</li>
+      </ul>
 
-    <h2>Doing the exercise</h2>
-    <p>This repo is based on the vanilla <code>create-react-app</code> template that is meant to help you get started, but feel free to use a different boilerplate or start with a clean slate.  You shouldn't need to, but you may also add additional NPM libraries as you like.  It is often just as important to be able to combine various open source libraries into a cohesive product.</p>
-    <p>It is preferred for you to complete this exercise using ReactJS, but if you are unfamiliar, you are also welcome to use other frameworks like Angular or Vue.</p>
+      <p>See <code>/examples/user_list.png</code> in the repo for a sample screenshot.</p>
 
-    <h2>Evaluation</h2>
+      <p>The user admin screen should</p>
 
-    <p>Your implementation will be evaluated against the following criteria:</p>
-    <ul>
-      <li>it has to work</li>
-      <li>cleanliness and readability of code</li>
-      <li>creativity - It's great to show off interesting implementations, but stick to the objectives.  This exercise should not take too long, so don't feel the need to add extra features.</li>
-    </ul>
+      <ul>
+        <li>Display each user's info in a simple format.  You can use a table, cards, or anything else you'd like.</li>
+      </ul>
+    </section>
+    <section>
+      <h2>Doing the exercise</h2>
+      <p>This repo is based on the vanilla <code>create-react-app</code> template that is meant to help you get started, but feel free to use a different boilerplate or start with a clean slate.  You shouldn't need to, but you may also add additional NPM libraries as you like.  It is often just as important to be able to combine various open source libraries into a cohesive product.</p>
+      <p>It is preferred for you to complete this exercise using ReactJS, but if you are unfamiliar, you are also welcome to use other frameworks like Angular or Vue.</p>
+    </section>
+    <section>
+      <h2>Evaluation</h2>
+
+      <p>Your implementation will be evaluated against the following criteria:</p>
+      <ul>
+        <li>it has to work</li>
+        <li>cleanliness and readability of code</li>
+        <li>creativity - It's great to show off interesting implementations, but stick to the objectives.  This exercise should not take too long, so don't feel the need to add extra features.</li>
+      </ul>
+    </section>
   </div >
 );
 
