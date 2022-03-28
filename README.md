a53024cb2745f42ff7b4dfff12ea4dccc0452965
Mike L <myclues@gmail.com>
2022-03-27 20:34:14 -0400

    add exercise

---
+++ README.md
@@ -0,0 +1,32 @@
+# Frontend Interview Exercise
+
+## What to code
+
+The exercise is to build a simple front-end web application that has two screens: a user sign-up form, and a user admin screen.
+
+The user sign-up form should
+
+- Accept the first name, last name, nickname, and email address
+- Each of the inputs is required except nickname
+- Validate the email - a simple rule that follows `x@y.z` format
+- A submission should be rejected if the email address is already in use
+
+The user admin screen should
+
+- Display each user's info in a simple format.  You can use a table, cards, or anything else you'd like.
+
+
+## Doing the exercise
+
+This repo is based on the vanilla `create-react-app` template that is meant to help you get started, but feel free to use a different boilerplate or start with a clean slate.  You shouldn't need to, but you may also add additional NPM libraries as you like.  It is often just as important to be able to combine various open source libraries into a cohesive product.
+
+It is preferred for you to complete this exercise using ReactJS, but if you are unfamiliar, you are also welcome to use other frameworks like Angular or Vue.
+
+
+## Evaluation
+
+Your implementation will be evaluated against the following criteria:
+
+- it has to work
+- cleanliness and readability of code
+- creativity - It's great to show off interesting implementations, but stick to the objectives.  This exercise should not take too long, so don't feel the need to add extra features.
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <h1>Frontend Interview Exercise</h1>
+
+    <h2>What to code</h2>
+    <p>The exercise is to build a simple front-end web application that has two screens: a user sign-up form, and a user admin screen.</p>
+
+    <p>The user sign-up form should</p>
+
+    <ul>
+      <li>Accept the first name, last name, nickname, and email address</li>
+      <li>Each of the inputs is required except nickname</li>
+      <li>Validate the email - a simple rule that follows <code>x@y.z</code> format</li>
+      <li>A submission should be rejected if the email address is already in use</li>
+    </ul>
+
+    <p>The user admin screen should</p>
+
+    <ul>
+      <li>Display each user's info in a simple format.  You can use a table, cards, or anything else you'd like.</li>
+    </ul>
+
+    <h2>Doing the exercise</h2>
+    <p>This repo is based on the vanilla <code>create-react-app</code> template that is meant to help you get started, but feel free to use a different boilerplate or start with a clean slate.  You shouldn't need to, but you may also add additional NPM libraries as you like.  It is often just as important to be able to combine various open source libraries into a cohesive product.</p>
+    <p>It is preferred for you to complete this exercise using ReactJS, but if you are unfamiliar, you are also welcome to use other frameworks like Angular or Vue.</p>
+
+    <h2>Evaluation</h2>
+
+    <p>Your implementation will be evaluated against the following criteria:</p>
+    <ul>
+      <li>it has to work</li>
+      <li>cleanliness and readability of code</li>
+      <li>creativity - It's great to show off interesting implementations, but stick to the objectives.  This exercise should not take too long, so don't feel the need to add extra features.</li>
+    </ul>
+  </div >
+);
+
+export default App;
